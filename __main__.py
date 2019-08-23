fcd5ebea01f43a80b84019cb0e37b5111962179a
root <root@instance-1.c.cardsite-200507.internal>
2019-08-23 17:04:58 +0000

    Forming in another way decorstor

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import os,sys
+
 from __args__ import mainVars
- 
+
+
 def _arguments_parser(arguments):
   try:
     _function_name=mainVars['actions_list'][arguments[0]]
@@ -12,43 +14,62 @@ def _arguments_parser(arguments):
   return _function_name
 
 
-def checkCredentials(function):
+def checkFunc(function):
 
   print("Inframig v%s" % mainVars['inframig_version'])
 
-  try:
-    AWS_KEY = os.environ['aws_access_key_id']
-    AWS_SECRET = os.environ['aws_secret_access_key']
-  except:
-    print("E! AWS credeitals weren't specified.\n")
-    print("E! Please be sure that \"aws_access_key_id\" and \"aws_secret_access_key\" variables were exported.")
-    sys.exit(1)
+  def wrappers():
 
-  def arguments_reader():
-    if len(sys.argv) == 1:
-      print("I! No actions were specified. Closed.")
-      sys.exit(0)
-    else:
-      function_id=_arguments_parser(sys.argv[1::])
-      return getattr(function(AWS_KEY, AWS_SECRET, sys.argv[1::]), mainVars['functions_list'][function_id])()
-
-  return arguments_reader()
+    def checkCredsWrapper():
+      try:
+        AWS_KEY = os.environ['aws_access_key_id']
+        AWS_SECRET = os.environ['aws_secret_access_key']
+      except:
+        print("E! AWS credeitals weren't specified.\n")
+        print("E! Please be sure that \"aws_access_key_id\" and \"aws_secret_access_key\" variables were exported.")
+        sys.exit(1)
+      return AWS_KEY, AWS_SECRET
 
 
-@checkCredentials
+    def argumentsWrapper():
+      if len(sys.argv) == 1:
+        print("I! No actions were specified. Closed.")
+        sys.exit(0)
+      else:
+        function_id=_arguments_parser(sys.argv[1::])
+        return function_id
+
+
+    def terraformCheckWrapper(system):
+        return 1.1
+
+
+    def executorWrapper():
+      system = os.uname()
+      AWS_KEY, AWS_SECRET = checkCredsWrapper()
+      function_id = argumentsWrapper()
+      version = terraformCheckWrapper(system)
+      return getattr(function(system, AWS_KEY, AWS_SECRET, sys.argv[1::]), mainVars['functions_list'][function_id])()
+
+    return executorWrapper()
+
+  return wrappers()
+
+@checkFunc
 class StartUp(object):
-  
-  def __init__(self, AWS_KEY, AWS_SECRET, command_line):
+ 
+  def __init__(self, system, AWS_KEY, AWS_SECRET, command_line):
     self.AWS_KEY = AWS_KEY
     self.AWS_SECRET = AWS_SECRET
     self.command_line = command_line
-    self.system = os.uname()
+
 
   def printVersion(self):
     print("Version: %s\nRelease date: %s\nWebsite: inframig.com" %
           (mainVars['inframig_version'],
           mainVars['inframig_release_date']))
     sys.exit(0)
+
 
   def printHelp(self):
     from __help__ import HelpMessage
